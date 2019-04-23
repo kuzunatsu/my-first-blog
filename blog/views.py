500d44ba2d0f6748af6888269e97ec7ec235733b
kuzunatsu <kuzuminatsuko@gmail.com>
2019-04-23 07:18:21 +0000

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+#from django.shortcuts import render
+
+
+#def post_list(request):
+	#return render(request, 'blog/post_list.html', {})
+	
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
